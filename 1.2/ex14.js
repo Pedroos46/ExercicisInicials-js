de9d1d01e5ccbd5340e23974db7f63dd40cd040b
Roger Pedrós Villorbina <pedrooos.46@gmail.com>
2016-11-06 23:39:56 +0100

    Si

--- 1.2/ex14.js
+++ 1.2/ex14.js
@@ -1,14 +1,17 @@
 var num = prompt("Entra el numero que del que vols el factorial: ");
 
-// 4! =  4 x 3 x 2 x 1 = 12. 
+// 4! =  4 x 3 x 2 x 1 = 24. 
 
 try{
     if(num <= 0) throw "No es pot fer el factorial de 0 o de un negatiu.";
     
-    var Numfactorial;
-    var result; 
-    
-    //FALTE AFERGIR ALGORITME DEF FACTORIAL!!!
+    var res = 1;
+    while(num>1){
+        res = res * num;
+        num--;
+    }
+    alert(res);
+
 }
 catch (err){
     alert(err);
